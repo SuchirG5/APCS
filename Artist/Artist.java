f3cae2e495e88845c1f2e1717cac69a242d575e6
Suchir <suchir.godavarthi@gmail.com>
2023-01-12 11:19:48 -0500

    commit

---
+++ Artist/Artist.java
@@ -0,0 +1,11 @@
+public class Artist implements Comparable<Artist>
+{
+    private String artistName;
+    private String 
+    @Override
+    public int compareTo(Artist o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
